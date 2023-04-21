425a3f06c2aa0400c852d0a99a5addae340c6ee3
careverca <airmincgn@gmail.com>
2023-04-21 15:45:02 +0700

    shortens header width on wide screens; opens links to reps in the new tabs

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -7,19 +7,19 @@ function Portfolio() {
       <h2 className="portfolio__heading">Портфолио</h2>
       <ul>
         <li>
-          <a className="portfolio__link" href="https://github.com/teleothleo/how-to-learn" >
+          <a className="portfolio__link" href="https://github.com/teleothleo/how-to-learn" target="_blank" rel="noreferrer">
             Статичный сайт
             <img src={arrow} className="portfolio__link-icon" alt="Link arrow" />
           </a>
         </li>
         <li>
-          <a className="portfolio__link" href="https://github.com/teleothleo/russian-travel" >
+          <a className="portfolio__link" href="https://github.com/teleothleo/russian-travel" target="_blank" rel="noreferrer">
             Адаптивный сайт
             <img src={arrow} className="portfolio__link-icon" alt="Link arrow" />
           </a>
         </li>
         <li>
-          <a className="portfolio__link portfolio__last-link" href="https://github.com/teleothleo/react-mesto-api-full-gha" >
+          <a className="portfolio__link portfolio__last-link" href="https://github.com/teleothleo/react-mesto-api-full-gha" target="_blank" rel="noreferrer">
             Одностраничное приложение
             <img src={arrow} className="portfolio__link-icon" alt="Link arrow" />
           </a>
